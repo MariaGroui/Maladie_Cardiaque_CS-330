3f676af240edfddf66d30899027a2a8438e8ce96
MariaGroui <44366160+MariaGroui@users.noreply.github.com>
2020-05-05 18:29:04 +0200

    Code up to date

---
+++ Code/resultValues.py
@@ -0,0 +1,26 @@
+from moteur_id3.id3 import ID3
+class ResultValues():
+
+    def __init__(self, donnees = None):
+        # Do computations here
+        self.donnees = donnees
+        # Task 1
+        id3 = ID3()
+        self.arbre = id3.construit_arbre(donnees)
+        # Task 3
+        self.faits_initiaux = None
+        self.regles = None
+        # Task 5
+        self.arbre_advance = None
+
+
+
+    def get_results(self):
+        return [self.arbre, self.faits_initiaux, self.regles, self.arbre_advance]
+
+    def get_profondeur(self):
+        """ parcourt l'arbre pour etidier sa profondeur
+        :return: un vecteur contenant la profondeur maximale et la profondeur \
+        maximale et la profondeur minimale
+        """
+
