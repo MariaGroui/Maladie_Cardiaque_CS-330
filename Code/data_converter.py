2c101644435fa58651fdd49b54ce44825a8f8898
MariaGroui <44366160+MariaGroui@users.noreply.github.com>
2020-05-07 12:23:57 +0200

    megachiantlerreure

--- Code/data_converter.py
+++ Code/data_converter.py
@@ -4,10 +4,16 @@ class data_converter():
     def __init__(self, fichier_donnees, classe_a_predire):
         self.donnees = []
 
+        print("coucou3")
+
         with open(fichier_donnees, mode='r', encoding='utf-8-sig') as csv_file:
+            print("coucou2")
             csv_reader = csv.DictReader(csv_file)
+            print("coucou1")
             for row in csv_reader:
+                print("coucouchiant")
                 attributs = {}
+                print("coucou")
                 for a in row:
                     if not a == classe_a_predire:
                         attributs[a] = row[a]
