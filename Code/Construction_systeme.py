b5e8abd8ef0cc92fe7bfdc99c275d1e3c2a3213c
MariaGroui <44366160+MariaGroui@users.noreply.github.com>
2020-05-07 12:01:06 +0200

    Projet up to date

--- Code/Construction_systeme.py
+++ Code/Construction_systeme.py
@@ -3,13 +3,13 @@ from data_converter import data_converter
 import sys
 
 if not ( len(sys.argv) == 3 or len(sys.argv) == 4):
-    print("Il faut entrer le nom d'un fichier contenant les données d'apprentissage",
-          "suivi du nom d'un fichier contenant les dosnnées à tester (facultatif)"
-          "et enfin, suivi du nom de la classe à prédire")
+    print("Il faut entrer le nom de la classe à prédire ",
+          "suivi du nom d'un fichier contenant les données d'apprentissage "
+          "et enfin, suivi du nom d'un fichier contenant les données à tester (facultatif) ")
 else:
 
-    classe = str(sys.argv[3])
-    fichier = str(sys.argv[1])
+    classe = str(sys.argv[1])
+    fichier = str(sys.argv[2])
     donnees = data_converter(fichier, classe).donnees
 
     #print(donnees)
@@ -22,21 +22,21 @@ else:
 
 if len(sys.argv) == 4:
 
-    fichier_test = str(sys.argv[2])
-    donnees_test = data_converter(fichier_test, classe).donnees_test
+    fichier_test = str(sys.argv[3])
+    donnees_test = data_converter(fichier_test, classe).donnees
     arbre_test = results[0]
 
     correct = 0
     iteration = 0
 
     for donnee in donnees_test :
-        classe = arbre_test.classifie(donnees_test)
-        if classe == donnee[0] :
+        classe = arbre_test.classifie(donnee)
+        if classe == donnee[0][0] :
             correct = correct + 1
 
         iteration = iteration + 1
 
-    print("Le pourcentage de bonne classification est: ")
+    print("Le pourcentage de précision (bonne classification) est: ")
     print( correct*100/iteration ) 
         
     
